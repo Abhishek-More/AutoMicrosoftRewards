7a404485dcc36646dc2f9aecc9cabd310e0270bb
Abhishek <abhimore74@gmail.com>
2020-10-23 12:05:05 -0400

    heroku

--- app.py
+++ app.py
@@ -3,24 +3,25 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate('autorewards.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://autorewards-92ae1.firebaseio.com/'
-})
 
-ref = db.reference('accounts')
-result = ref.get()
-
-accounts = []
-if result:
-    for key, value in result.items():
-        accounts.append((key, value))
 
 
 app = Flask('__main__')
 
 @app.route("/")
 def home():
+    cred = credentials.Certificate('autorewards.json')
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://autorewards-92ae1.firebaseio.com/'
+    })
+
+    ref = db.reference('accounts')
+    result = ref.get()
+
+    accounts = []
+    if result:
+        for key, value in result.items():
+            accounts.append((key, value))
     string = ""
     for account in accounts:
         string += str(account[0]) + ": " + str(account[1]) + "\n"
